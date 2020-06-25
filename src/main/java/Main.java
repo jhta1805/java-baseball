6e47729d0c4f5bf3988d3be944490ed1044379e8
jslee <jslee@epinet.kr>
2020-06-25 17:36:30 +0900

    코드 정리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Computer c = new Computer();
-        Boolean scannerChk = true;
         String userInput = "";
         System.out.println("1부터 9까지로 이루어진 숫자 3개를 눌러주세요.\n더 이상 게임을 원하지 않을경우 000 을 입력하세요.");
-        while (scannerChk) {
+        while (true) {
             String computerInput = c.initNumber();
             Scanner sc = new Scanner(System.in);
             System.out.println("숫자를 입력하세요.");
             userInput = sc.next();
             if (userInput.equals("000")) {
-                scannerChk = false;
                 break;
             }
             if (userInput.length() != 3) {
@@ -21,7 +19,6 @@ public class Main {
             }
             System.out.println(c.checkNumber(userInput, computerInput));
             System.out.println("상대방 숫자는 " + computerInput + "입니다.");
-
         }
         System.out.println("게임이 종료됐습니다. 감사합니다.");
     }
