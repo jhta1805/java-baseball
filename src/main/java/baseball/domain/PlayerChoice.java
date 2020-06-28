ee13756c383773c1aa05cfbb31f258b40eb19739
taejinseok <stj930@gmail.com>
2020-06-28 21:15:04 +0900

    feat: 게임 종료 여부 구분 기능 구현

---
+++ src/main/java/baseball/domain/PlayerChoice.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+import java.util.Arrays;
+
+public enum PlayerChoice {
+    PLAY_AGAIN("1"), GAME_END("2");
+
+    private static final String INVALID_PLAYER_CHOICE_CODE_EXCEPTION_MESSAGE = "유효한 값이 아닙니다.";
+
+    private final String chooseCode;
+
+    PlayerChoice(String chooseCode) {
+        this.chooseCode = chooseCode;
+    }
+
+    public static PlayerChoice of(String value) {
+        return Arrays.stream(values())
+            .filter(playerChoiceValue -> playerChoiceValue.chooseCode.equals(value))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(INVALID_PLAYER_CHOICE_CODE_EXCEPTION_MESSAGE));
+    }
+
+    public boolean isPlayAgain() {
+        return this == PLAY_AGAIN;
+    }
+}
