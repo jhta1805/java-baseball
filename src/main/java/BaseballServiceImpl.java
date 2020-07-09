cefdd02bedbf8d20e9db43c2f381d4e5e624f5ed
jslee <jslee@epinet.kr>
2020-07-09 12:12:48 +0900

    피드백 받은거 정리
    1. 축약된 변수명 변경
    bs -> baseballGame, bcnt -> ballCnt, scnt -> strikeCnt
    
    2. 상수화
    private final 로 변경
    
    3. BaseballService 의 이름 변경
    SportsGameService 로 이름 변경
    이유 : 나중에 야구 말고 다른게임이 나왔을경우 확장하고 싶어서

--- src/main/java/BaseballServiceImpl.java
+++ src/main/java/BaseballServiceImpl.java
@@ -1,4 +1,4 @@
-public class BaseballServiceImpl implements BaseballService {
+public class BaseballServiceImpl implements SportsGameService {
 
     @Override
     public String playGame(String userNumber) {
@@ -9,69 +9,71 @@ public class BaseballServiceImpl implements BaseballService {
         return checkNumber(b.getUserNumber(), b.getComNumber());
     }
 
-
+    private final Integer NUM_DIGIT = 3;
+    private final String OVER_DIGIT_MESSAGE = "세자리 수를 입력해주세요.";
     @Override
     public Boolean checkLength(String userNumber) {
-        if (userNumber.length() != 3) {
-            System.out.println("세자리 수를 입력해주세요.");
+        if (userNumber.length() != NUM_DIGIT) {
+            System.out.println(OVER_DIGIT_MESSAGE);
             return false;
         }
         return true;
     }
-
+    private final Integer GAME_QUIT = 000;
     @Override
     public Boolean gameQuit(String userNumber) {
-        if (userNumber.equals("000")) {
+        if (userNumber.equals(GAME_QUIT)) {
             return false;
         }
         return true;
     }
 
+    private final String NOT_INT_MESSAGE = "문자는 안됩니다. 숫자를 입력하세요.";
     @Override
     public Boolean isInt(String userNumber) {
         try {
             Integer.parseInt(userNumber);
             return true;
         } catch (NumberFormatException e) {
-            System.out.println("문자는 안됩니다. 숫자를 입력하세요.");
+            System.out.println(NOT_INT_MESSAGE);
             return false;
         }
 
     }
 
-    public String checkNumber(String userNumber, String comNumber) {
-        int bcnt = 0;
-        int scnt = 0;
+    private String checkNumber(String userNumber, String comNumber) {
+        int ballCount = 0;
+        int strikeCount = 0;
         int cn = Integer.parseInt(comNumber);
         for (int i = 0; i < comNumber.length(); i++) {
             String kcn = cn % 10 + "";
             int chk = reverseStr(userNumber).lastIndexOf(kcn);
             if (chk > -1 && chk != i) {
-                bcnt++;
+                ballCount++;
             }
             if (chk == i) {
-                scnt++;
+                strikeCount++;
             }
             cn = cn / 10;
         }
-        return result(bcnt, scnt);
+        return result(ballCount, strikeCount);
     }
 
-    public String result(int bCnt, int sCnt) {
+    private String result(int ballCount, int strikeCount) {
         String res = "";
-        if (bCnt > 0) {
-            res += bCnt + " ball ";
+        if (ballCount > 0) {
+            res += ballCount + " ball ";
         }
-        if (sCnt > 0) {
-            res += sCnt + " strike";
+        if (strikeCount > 0) {
+            res += strikeCount + " strike";
         }
-        if (bCnt == 0 && sCnt == 0) {
+        if (ballCount == 0 && strikeCount == 0) {
             res = "nothing";
         }
         return res;
     }
 
-    public String getRandomInteger(int maximum, int minimum, int length) {
+    private String getRandomInteger(int maximum, int minimum, int length) {
         String res = "";
         while(!(res.length() == length)) {
             int num = ((int) (Math.random() * (maximum - minimum))) + minimum;
@@ -83,7 +85,7 @@ public class BaseballServiceImpl implements BaseballService {
     }
 
 
-    public static String reverseStr(String s) {
+    private static String reverseStr(String s) {
         return (new StringBuffer(s)).reverse().toString();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BaseballService bs = new BaseballServiceImpl();
+        SportsGameService baseballGame = new BaseballServiceImpl();
         System.out.println("1부터 9까지로 이루어진 숫자 3개를 눌러주세요.\n더 이상 게임을 원하지 않을경우 000 을 입력하세요.");
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("숫자를 입력하세요.");
             String userNumber = sc.next();
-            if (!bs.gameQuit(userNumber)) {
+            if (!baseballGame.gameQuit(userNumber)) {
                 break;
             }
-            if (!bs.isInt(userNumber) || !bs.checkLength(userNumber)) {
+            if (!baseballGame.isInt(userNumber) || !baseballGame.checkLength(userNumber)) {
                 continue;
             }
-            System.out.println(bs.playGame(userNumber));
+            System.out.println(baseballGame.playGame(userNumber));
         }
         System.out.println("게임이 종료됐습니다. 감사합니다.");
     }
